d89964b326c6c0bc5938a3d949743858e0844f7d
Magnus Enger <magnus@enger.priv.no>
2011-04-18 18:27:33 +0200

    Basic working version, needs some tuning

--- oai2mw.pl
+++ oai2mw.pl
@@ -1,55 +1,255 @@
-#!/opt/local/bin/perl -w
+#!/usr/bin/perl -w
 
-use Net::OAI::Harvester; 
+use Net::OAI::Harvester;
+use MediaWiki::Bot;
+use Template;
 use Data::Dumper;
+use Getopt::Long;
+use Pod::Usage;
 use strict;
 
-# create a harvester for the Library of Congress
+my ($baseurl, $all, $record, $limit, $info, $verbose, $debug) = get_options();
+
+# Create a harvester
 my $harvester = Net::OAI::Harvester->new( 
-    'baseURL' => 'http://telemarkskilder1.hit.no/dspace-oai/request'
+    'baseURL' => $baseurl,
 );
 
-## Find out the name for a repository
-# my $identity = $harvester->identify();
-# print "name: ",$identity->repositoryName(),"\n";
+# Set up Template Toolkit
+my $ttconfig = {
+    INCLUDE_PATH => '.',  # or list ref
+    INTERPOLATE  => 1,    # expand "$var" in plain text
+    POST_CHOMP   => 0,    # cleanup whitespace 
+};
+my $tt2 = Template->new($ttconfig) || die Template->error(), "\n";
 
-## List sets
-# my $sets = $harvester->listSets();
-# foreach ( $sets->setSpecs() ) { 
-#   print "set spec: $_ ; set name: ", $sets->setName( $_ ), "\n";
-# }
+# Set up a Mediawiki bot 
+my $bot = MediaWiki::Bot->new({
+	assert      => 'bot',
+}) or die "Could not create bot";
+$bot->set_wiki({
+	host        => 'dt-test-wiki.websites.jonnybe.webdev.hit.no',
+	path        => '',
+}) or die "Could not set wiki";
+# TODO make this configurable
+$bot->login({
+	username => 'Oai2mw',
+	password => '',
+}) or die "Login failed ", $bot->{'error'}->{'code'}, " ", $bot->{'error'}->{'details'};
 
-## List metadataformats
-# my $list = $harvester->listMetadataFormats();
-# print "archive supports metadata prefixes: ", join( ',', $list->prefixes() ),"\n";
+if ($info) {
 
-## Get info about 1 record
-# Web:     http://telemarkskilder1.hit.no/dspace/handle/123456789/2176?mode=full&submit_simple=Vis+fullstendig+innf%C3%B8rsel
-# OAI-PMH: http://telemarkskilder1.hit.no/dspace-oai/request?verb=GetRecord&identifier=oai:telemarkskilder1.hit.no:123456789/2176&metadataPrefix=oai_dc
-my $record = $harvester->getRecord( 
-  identifier      => 'oai:telemarkskilder1.hit.no:123456789/2176',
-  'metadataPrefix'    => 'oai_dc',
+	# If $info is set, we print some info about the repo and then exit
+
+	# Find out the name for a repository
+	my $identity = $harvester->identify();
+	print "* Name: ",$identity->repositoryName(),"\n";
+
+	# List sets
+	my $sets = $harvester->listSets();
+	print "\n* Sets\n";
+	foreach ( $sets->setSpecs() ) { 
+		print "set spec: $_ ; set name: ", $sets->setName( $_ ), "\n";
+	}
+
+	# List metadataformats
+	print "\n* Metadata formats: ";
+	my $list = $harvester->listMetadataFormats();
+	print join( ',', $list->prefixes() ),"\n\n";
+
+  exit;
+
+}
+
+if ($all) {
+
+	# List all the records in the repository
+	my $records = $harvester->listAllRecords( 
+		  'metadataPrefix'    => 'oai_dc' 
+	);
+	my $count = 0;
+	while ( my $record = $records->next() ) {
+		my $header = $record->header();
+		my $metadata = $record->metadata();
+		if ($metadata->title()) {
+			print $header->identifier(), ": ", $metadata->title(), "\n";
+			$count++;
+		}
+    # Honour the --limit option
+		if ($count == $limit) {
+			last;
+		}
+	}
+	print "$count records\n";
+
+	exit;
+
+}
+
+if ($record) {
+
+	# Get info about 1 record
+	my $rec = $harvester->getRecord( 
+	'identifier'     => $record,
+	'metadataPrefix' => 'oai_dc',
+	);
+
+	# Dump the Net::OAI::Record::Header object
+	# my $header = $rec->header();
+	# print Dumper $header;
+
+	# Dump the metadata object 
+	my $metadata = $rec->metadata();
+	print Dumper $metadata;
+
+	exit;
+
+}
+
+# Walk through all the records in the repository, respecting --limit
+my $records = $harvester->listAllRecords( 
+	  'metadataPrefix'    => 'oai_dc' 
 );
+my $count = 0;
+while ( my $record = $records->next() ) {
+	my $header = $record->header();
+	my $metadata = $record->metadata();
+	if ($metadata->title()) {
+		my %record = metadata2structure($metadata);
+		# Output
+		my $text = '';
+		$tt2->process('mwtemplate.tt', { 'rec' => \%record }, \$text) || die $tt2->error();
+		$bot->edit({
+			page    => $metadata->title(),
+			text    => $text,
+			summary => 'Lagt inn av oai2mw',
+			# section => 'new',
+		});
+		$count++;
+	}
+  # Honour the --limit option
+	if ($count == $limit) {
+		last;
+	}
+}
+print "$count records\n";
 
-## get the Net::OAI::Record::Header object
-my $header = $record->header();
-print Dumper $header;
+### SUBROUTINES
 
-## get the metadata object 
-my $metadata = $record->metadata();
-print Dumper $metadata;
+sub metadata2structure {
+
+	my $meta = shift;
+	my %data;
+
+	# Misc data
+	$data{'title'}        = $meta->title();
+	$data{'publisher'}    = $meta->publisher();
+	$data{'language'}     = $meta->language();
+	$data{'description'}  = $meta->description();
+	$data{'relation'}     = $meta->relation();
+	# TODO Make the word used for "Kategori" configurable
+	$data{'relation_cat'} = '[[Kategori:' . ucfirst($meta->relation()) . ']] ';
+
+	# Identifier
+	my $identifier    = $meta->identifier();
+	$identifier =~ m/.*\/([0-9]{1,9})$/;
+	$data{'number'} = $1;
+
+	# Coverage
+	my @coverage = $meta->coverage();
+	foreach my $cover (@coverage) {
+		# Skip anything that starts with a number
+		if ($cover =~ m/^([^0-9].*)$/i) {
+			next if (substr($1, 0, 3) eq 'UTM');
+			$data{'coverage'} .= $1 . ", ";
+			$data{'coverage_cat'} .= '[[Kategori:' . ucfirst($1) . ']] ';
+		}
+	}
+
+	# Subjects
+	my @subjects = $meta->subject();
+	foreach my $subject (@subjects) {
+		$data{'subjects'} .= $subject . ", ";
+		$data{'subjects_cat'} .= '[[Kategori:' . ucfirst($subject) . ']] ';
+	}
+
+	return %data;
+
+}
+
+# Get commandline options
+sub get_options {
+  my $baseurl     = '';
+	my $all         = '';
+	my $record      = '';
+  my $limit       = 0;
+  my $info        = '';
+  my $verbose     = '';
+  my $debug       = '';
+  my $help        = '';
+
+  GetOptions("b|baseurl=s" => \$baseurl,
+             "a|all"       => \$all, 
+             "r|record=s"  => \$record, 
+             "l|limit=i"   => \$limit,
+             "i|info"      => \$info,
+             "v|verbose"   => \$verbose,
+             "d|debug"     => \$debug,
+             "h|help"      => \$help,
+             );
+  
+  pod2usage(-exitval => 0) if $help;
+  pod2usage( -msg => "\nMissing Argument: -b, --baseurl required\n", -exitval => 1) if !$baseurl;
+
+  return ($baseurl, $all, $record, $limit, $info, $verbose, $debug);
+}       
 
 __END__
 
-## list all the records in a repository
-my $records = $harvester->listAllRecords( 
-    'metadataPrefix'    => 'oai_dc' 
-);
-while ( my $record = $records->next() ) {
-    my $header = $record->header();
-    my $metadata = $record->metadata();
-    print "identifier: ", $header->identifier(), "\n";
-    print "title: ", $metadata->title(), "\n";
-}
+=head1 NAME
+    
+oai2mw.pl - Copy metadata from an OAI-PMH repository to a MediaWiki wiki
+        
+=head1 SYNOPSIS
+            
+oai2mw.pl --info
 
-## GetRecord, ListSets, ListMetadataFormats also supported
+=head1 OPTIONS
+              
+=over 8
+
+=item B<-b, --baseurl>
+
+Base URL of the OAI repo we want to talk to, e.g. http://example.org/dspace-oai/request 
+
+=item B<-a, --all>
+
+List the identifier and title of all records with a title. Can be combined with --limit.  
+
+=item B<-r, --record>
+
+Dump all info about one record identified by the given ID. 
+
+=item B<-l, --limit>
+
+Max number of records to handle. 
+
+=item B<-i, --info>
+
+Display misc info about the given repository. 
+
+=item B<-v, --verbose>
+
+Turn on verbose output. 
+
+=item B<-d, --debug>
+
+Turn on debug output. 
+
+=item B<-h, --help>
+
+Print this documentation. 
+
+=back
+                                                               
+=cut
